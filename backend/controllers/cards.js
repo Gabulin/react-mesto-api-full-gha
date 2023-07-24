e09f99d592a524c243fd7944976803729691965e
Gabulin <andrew_bd_github@mail.ru>
2023-07-25 03:11:42 +0700

    fix: controllers

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,16 +1,18 @@
-const Card = require('../models/card');
-const InvalidError = require('../errors/InvalidError');
-const NotFoundError = require('../errors/NotFoundError');
-const ForbiddenError = require('../errors/ForbiddenError');
-const { MESSAGE_ERROR_NOT_FOUND } = require('../utils/Constants');
+const Card = require("../models/card");
+const InvalidError = require("../errors/InvalidError");
+const NotFoundError = require("../errors/NotFoundError");
+const ForbiddenError = require("../errors/ForbiddenError");
+const { MESSAGE_ERROR_NOT_FOUND } = require("../utils/Constants");
 
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => card.populate('owner').then((data) => res.status(201).send(data)))
+    .then((card) =>
+      card.populate("owner").then((data) => res.status(201).send(data))
+    )
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return next(new InvalidError('Неккоректные данные'));
+      if (err.name === "ValidationError") {
+        return next(new InvalidError("Некорректные данные"));
       }
       return next(err);
     });
@@ -18,87 +20,72 @@ const createCard = (req, res, next) => {
 
 const getCards = (req, res, next) => {
   Card.find({})
-    .populate('likes')
     .then((cards) => res.send(cards))
     .catch(next);
 };
 
 const deleteCard = (req, res, next) => {
-  try {
-    Card.findById(req.params.cardId)
-      .then((card) => {
-        if (!card) {
-          throw new NotFoundError(MESSAGE_ERROR_NOT_FOUND);
-        }
+  Card.findById(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError(MESSAGE_ERROR_NOT_FOUND);
+      }
 
-        if (card.owner.equals(req.user._id)) {
-          return card.deleteOne().then(() => res.send(card));
-        }
+      if (card.owner.equals(req.user._id)) {
+        return card.deleteOne().then(() => res.send(card));
+      }
 
-        return next(new ForbiddenError('Некорректные данные для удаления'));
-      })
-      .catch((err) => {
-        if (err.name === 'CastError') {
-          res.status(400).json({ error: 'Неверный формат идентификатора' });
-        } else {
-          next(err);
-        }
-      });
-  } catch (err) {
-    next(err);
-  }
+      return next(new ForbiddenError("Некорректные данные для удаления"));
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        next(new InvalidError("Неверный формат идентификатора"));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const likeCard = (req, res, next) => {
-  try {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true },
-    )
-      .populate(['likes', 'owner'])
-      .then((card) => {
-        if (!card) {
-          throw new NotFoundError(MESSAGE_ERROR_NOT_FOUND);
-        }
-        res.send(card);
-      })
-      .catch((err) => {
-        if (err.name === 'CastError') {
-          res.status(400).json({ error: 'Неверный формат идентификатора' });
-        } else {
-          next(err);
-        }
-      });
-  } catch (err) {
-    next(err);
-  }
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError(MESSAGE_ERROR_NOT_FOUND);
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        next(new InvalidError("Неверный формат идентификатора"));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const dislikeCard = (req, res, next) => {
-  try {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: req.user._id } },
-      { new: true },
-    )
-      .populate('owner')
-      .then((card) => {
-        if (!card) {
-          throw new NotFoundError(MESSAGE_ERROR_NOT_FOUND);
-        }
-        res.send(card);
-      })
-      .catch((err) => {
-        if (err.name === 'CastError') {
-          res.status(400).json({ error: 'Неверный формат идентификатора' });
-        } else {
-          next(err);
-        }
-      });
-  } catch (err) {
-    next(err);
-  }
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError(MESSAGE_ERROR_NOT_FOUND);
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        next(new InvalidError("Неверный формат идентификатора"));
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports = {
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,10 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const RegisterError = require('../errors/RegisterError');
-const NotFoundError = require('../errors/NotFoundError');
-const InvalidError = require('../errors/InvalidError');
-const { JWT_KEY } = require('../utils/Constants');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const RegisterError = require("../errors/RegisterError");
+const NotFoundError = require("../errors/NotFoundError");
+const InvalidError = require("../errors/InvalidError");
+const { JWT_KEY } = require("../utils/Constants");
 
 const getUsers = (req, res, next) => {
   User.find({})
@@ -13,31 +13,33 @@ const getUsers = (req, res, next) => {
 };
 
 const createUser = (req, res, next) => {
-  const {
-    name, about, avatar, email, password,
-  } = req.body;
+  const { name, about, avatar, email, password } = req.body;
 
   bcrypt
     .hash(password, 10)
-    .then((hash) => User.create({
-      name,
-      about,
-      avatar,
-      email,
-      password: hash,
-    }))
-    .then((user) => res.status(201).send({
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      email: user.email,
-    }))
+    .then((hash) =>
+      User.create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      })
+    )
+    .then((user) =>
+      res.status(201).send({
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+      })
+    )
     .catch((err) => {
       if (err.code === 11000) {
-        return next(new RegisterError('Пользователь уже существует'));
+        return next(new RegisterError("Пользователь уже существует"));
       }
-      if (err.name === 'ValidationError') {
-        return next(new InvalidError('Неккоректные данные'));
+      if (err.name === "ValidationError") {
+        return next(new InvalidError("Неккоректные данные"));
       }
       return next(err);
     });
@@ -49,10 +51,10 @@ const login = (req, res, next) => {
   User.findByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_KEY, {
-        expiresIn: '7d',
+        expiresIn: "7d",
       });
 
-      res.cookie('jwt', token, {
+      res.cookie("jwt", token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
       });
@@ -63,31 +65,26 @@ const login = (req, res, next) => {
 };
 
 const getUserById = (req, res, next) => {
-  try {
-    User.findById(req.params.userId)
-      .then((user) => {
-        if (!user) {
-          throw new NotFoundError('Пользователь не найден');
-        }
-        return res.send(user);
-      })
-      .catch((err) => {
-        if (err.name === 'CastError') {
-          res.status(400).json({ error: 'Неверный формат идентификатора пользователя' });
-        } else {
-          next(err);
-        }
-      });
-  } catch (err) {
-    next(err);
-  }
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError("Пользователь не найден");
+      }
+      return res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return next(new InvalidError("Неккоректные данные"));
+      }
+      return next(err);
+    });
 };
 
 const getUser = (req, res, next) => {
   User.findById(req.user._id)
     .then((user) => {
       if (!user) {
-        return next(new NotFoundError('Пользователь не найден'));
+        return next(new NotFoundError("Пользователь не найден"));
       }
 
       return res.send(user);
@@ -101,12 +98,12 @@ const updateProfile = (req, res, next) => {
   return User.findByIdAndUpdate(
     req.user._id,
     { name, about },
-    { new: true, runValidators: true },
+    { new: true, runValidators: true }
   )
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return next(new InvalidError('Неккоректные данные'));
+      if (err.name === "ValidationError") {
+        return next(new InvalidError("Неккоректные данные"));
       }
 
       return next(err);
@@ -116,11 +113,14 @@ const updateProfile = (req, res, next) => {
 const updateAvatar = (req, res, next) => {
   const data = { avatar: req.body.avatar };
 
-  return User.findByIdAndUpdate(req.user._id, data, { runValidators: true, new: true })
+  return User.findByIdAndUpdate(req.user._id, data, {
+    runValidators: true,
+    new: true,
+  })
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return next(new InvalidError('Неккоректные данные ссылки'));
+      if (err.name === "ValidationError") {
+        return next(new InvalidError("Неккорректные данные ссылки"));
       }
 
       return next(err);
